19ec6a811e999f6e948882d23b4442bf6f6389e2
James Labocki <jlabocki@redhat.com>
2022-02-24 10:57:14 -0500

    learning

---
+++ tweet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import tweepy
+from time import sleep
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+api.update_status(status = "Testing Tweeter!")
+
