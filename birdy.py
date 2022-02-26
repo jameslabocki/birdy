29716a1a1a70a82f4d546fbe9aac3a9a6fbde9ba
James Labocki <jlabocki@redhat.com>
2022-02-26 13:21:53 -0500

    optimizations

--- birdy.py
+++ birdy.py
@@ -3,29 +3,31 @@
 from picamera import PiCamera
 import tweepy
 import time
+from datetime import datetime
 from time import sleep
 from credentials import *
 from gpiozero import MotionSensor
 from signal import pause
 
-
 pir = MotionSensor(27)
-
 camera = PiCamera()
-
 filename = "null"
 
 def takepicture():
-    filename = "/home/pi/birdyshots/picture_" + str(time.time()) + ".jpg"
+    ts = time.time()
+    filename = "/home/pi/birdyshots/picture_" + str(ts) + ".jpg"
+    print("Taking Picture " + filename)
+    camera.start_preview()
     camera.capture(filename)
-    sleep(1)
+    camera.stop_preview()
+    sleep(5)
     return filename
 
 def tweetpicture(picturepath):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    tweet = "Bird Detected" + str(time.time())
+    tweet = "Bird Detected " + datetime 
     status = api.update_with_media(picturepath, tweet)
     api.update_status(status = tweet)
 
